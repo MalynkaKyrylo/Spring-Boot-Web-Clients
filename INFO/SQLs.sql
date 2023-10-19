fbee4bc69d13d4bd8bb43b71c86fbb8d68d34a74
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-10-19 23:37:29 +0300

    Fix file name in SQL promt

--- INFO/SQLs.sql
+++ INFO/SQLs.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS clients
 INSERT INTO
 	clients (first_name, last_name, phone, image, description)
 VALUES
-	('Tom', 'Jefferson', '777 555-5555', 'huawei_mate_x3,jpg', 'Huawei Mate X3'),
+	('Tom', 'Jefferson', '777 555-5555', 'huawei_mate_x3.jpg', 'Huawei Mate X3'),
 	('Bob', 'Green', '777 333-3333', 'iphone_15.jpg', 'iPhone 15'),
 	('Andy', 'Wallace', '777 222-2222', 'samsung_zfold5.jpg', 'Samsung ZFold5'),
 	('Emma', 'Grace', '777 111-1234', 'xiaomi_m12.jpg', 'Xiaomi M12');
