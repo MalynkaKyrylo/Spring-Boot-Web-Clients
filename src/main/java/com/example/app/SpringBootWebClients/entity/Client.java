42cefd2d4f61203786ef1f427c47166f6438770b
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-10-19 22:06:55 +0300

    Refactor Entity to Client

--- src/main/java/com/example/app/SpringBootWebClients/entity/Client.java
+++ src/main/java/com/example/app/SpringBootWebClients/entity/Client.java
@@ -1,4 +1,4 @@
-package com.example.app.SpringBootWebTours.entity;
+package com.example.app.SpringBootWebClients.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -7,16 +7,20 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "places")
-public class Place {
+@Table(name = "clients")
+public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "phone")
+    private String phone;
     @Column(name = "image")
     private String img;
-    @Column(name = "name")
-    private String name;
     @Column(name = "description")
     private String descr;
 }
