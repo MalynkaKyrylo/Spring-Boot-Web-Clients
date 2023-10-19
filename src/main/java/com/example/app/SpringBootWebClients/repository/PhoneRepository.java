2fe52b569b9ffc3a41e92417382696b843bb529a
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-10-19 22:12:34 +0300

    Refactor PhoneRepository class

--- src/main/java/com/example/app/SpringBootWebClients/repository/PhoneRepository.java
+++ src/main/java/com/example/app/SpringBootWebClients/repository/PhoneRepository.java
@@ -1,7 +1,7 @@
-package com.example.app.SpringBootWebTours.repository;
+package com.example.app.SpringBootWebClients.repository;
 
-import com.example.app.SpringBootWebTours.entity.Resort;
-import com.example.app.SpringBootWebTours.utils.Constants;
+import com.example.app.SpringBootWebClients.entity.Phone;
+import com.example.app.SpringBootWebClients.utils.Constants;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.core.io.ClassPathResource;
@@ -17,32 +17,32 @@ import java.util.List;
 import java.util.logging.Logger;
 
 @Repository
-public class ResortRepository {
+public class PhoneRepository {
 
     private static final Logger LOGGER =
-            Logger.getLogger(ResortRepository.class.getName());
+            Logger.getLogger(PhoneRepository.class.getName());
 
-    List<Resort> list;
+    List<Phone> list;
 
-    public List<Resort> getResorts() throws IOException {
+    public List<Phone> getPhones() throws IOException {
 
         File jsonFile = new ClassPathResource(Constants.URL_FILES +
-                Constants.FILE_RESORTS).getFile();
+                Constants.FILE_PHONES).getFile();
         String path = jsonFile.getAbsolutePath();
 
         try {
             Gson gson = new Gson();
             Reader reader = Files.newBufferedReader(Paths.get(path));
             list = gson.fromJson(reader,
-                    new TypeToken<List<Resort>>() {}.getType());
+                    new TypeToken<List<Phone>>() {}.getType());
             reader.close();
             return list.stream()
-                    .map(tour -> new Resort(tour.getId(),
-                            Constants.URL_IMAGES + tour.getImg(),
-                            tour.getDescr()))
+                    .map(phone -> new Phone(phone.getId(),
+                            Constants.URL_IMAGES + phone.getImg(),
+                            phone.getDescr()))
                     .toList();
         } catch (Exception ex) {
-            LOGGER.info("ResortRepository msg: " + ex.getMessage());
+            LOGGER.info("PhoneRepository msg: " + ex.getMessage());
             // Якщо помилка, повертаємо порожню колекцію
             return Collections.emptyList();
         }
