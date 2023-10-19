8053d3fa4582664ccd52792b5f10f672089808c0
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-10-19 22:04:48 +0300

    Add SQL promts

--- INFO/SQLs.sql
+++ INFO/SQLs.sql
@@ -1,37 +1,24 @@
-
--- БАЗА ДАНИХ
--- Можливість створення БД з метою уникнення некваліфікованих
--- дій, краще залишити за розробником.
--- Тому такий функціонал у додатку не прописуємо.
--- Можемо створити БД через візуальний інструмент, наприклад,
--- MySQL Workbench.
 CREATE DATABASE demo_db;
 
-
--- ТАБЛИЦІ
--- Можливість створення таблиць БД, з метою уникнення некваліфікованих
--- дій, краще залишити за розробником.
--- Тому такий функціонал у додатку не прописуємо.
--- Попередньо, необхідно спроектувати таблиці та їх зв'язки,
--- на основі сутностей реального світу.
--- Можемо створити таблиці БД через візуальний інструмент, наприклад,
--- MySQL Workbench.
-
-
-CREATE TABLE IF NOT EXISTS places
+CREATE TABLE IF NOT EXISTS clients
 ( id INTEGER NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  phone VARCHAR(128) NOT NULL,
   image VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
   description VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
 
 INSERT INTO
-	places (image, name, description)
+	clients (first_name, last_name, phone, image, description)
 VALUES
-	('azure-beach.jpg', 'Azure beach', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
-	('sunny-club.jpg', 'Sunny Club', 'Donec tempor mi nulla, eu aliquam massa rutrum eu.'),
-	('fish-farm.jpg', 'Fish farm', 'Nam tempus ultrices mauris.');
+	('Tom', 'Jefferson', '777 555-5555', 'huawei_mate_x3,jpg', 'Huawei Mate X3'),
+	('Bob', 'Green', '777 333-3333', 'iphone_15.jpg', 'iPhone 15'),
+	('Andy', 'Wallace', '777 222-2222', 'samsung_zfold5.jpg', 'Samsung ZFold5'),
+	('Emma', 'Grace', '777 111-1234', 'xiaomi_m12.jpg', 'Xiaomi M12');
+
+SELECT first_name, last_name, phone, image, description FROM clients;
 
 
